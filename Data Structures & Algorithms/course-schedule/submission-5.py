8d670ba78ed9239b81b9fd71c9f6aea3ac4b7e4b
Enrique Fernández <73256011+EnriqueFdez03@users.noreply.github.com>
2026-06-14 23:59:06 +0200

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # kahn algorithm, topological sort
+        # adj list
+        adj = {i: [] for i in range(numCourses)}
+        indegree = [0] * numCourses
+
+        for course, pre in prerequisites:
+            adj[pre].append(course)
+            indegree[course] += 1
+        
+        queue = deque([])
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        processed = 0
+        while queue:
+            pre = queue.popleft()
+            processed += 1
+            for course in adj[pre]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+        
+        return processed == numCourses
