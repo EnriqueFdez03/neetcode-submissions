358b2be3ec59a334be75a6cbedd76286815bd4fd
Enrique Fernández <73256011+EnriqueFdez03@users.noreply.github.com>
2026-07-06 22:32:24 +0200

    Add: decode-ways - submission-19

---
+++ Data Structures & Algorithms/decode-ways/submission-19.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # >= 27 or starts with 0 -> not a mapping 
+        n = len(s)
+        cache = [-1] * n
+        def dfs(i):
+            if i == n:
+                return 1
+            if i > n:
+                return 0
+            if cache[i] != -1:
+                return cache[i]
+            
+            if s[i] == "0":
+                cache[i] = 0
+                return cache[i]
+
+            res = dfs(i + 1)
+            if s[i] == "1" or (i < n - 1 and s[i] == "2" and s[i + 1] in "0123456"):
+                res += dfs(i + 2)
+            
+            cache[i] = res
+            return cache[i]
+        
+        return dfs(0)
