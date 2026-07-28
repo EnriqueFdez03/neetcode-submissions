ba306ed045ceede5beced8e0efeca6e77883abcf
Enrique Fernández <73256011+EnriqueFdez03@users.noreply.github.com>
2026-07-28 23:43:22 +0200

    Add: car-fleet - submission-10

---
+++ Data Structures & Algorithms/car-fleet/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # play with a stack
+        # position and speed, reverse sort based on position
+        posAndSpeed = list(zip(position, speed))
+        posAndSpeed.sort(reverse=True)
+
+        stack = []
+        for (p, s) in posAndSpeed:
+            timeToReach = (target - p) / s
+            if not stack:
+                stack.append(timeToReach)
+            else:
+                nextCarTime = stack[-1]
+                if timeToReach > nextCarTime:
+                    stack.append(timeToReach)
+        
+        return len(stack)
