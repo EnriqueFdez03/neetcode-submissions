fc9ec73176a939b851815297bdf43c7dbf550a87
Enrique Fernández <73256011+EnriqueFdez03@users.noreply.github.com>
2026-08-07 23:57:12 +0200

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        mapping = {
+            "2": "abc", "3": "def", "4": "ghi",
+            "5": "jkl", "6": "mno",
+            "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+        res = []
+        
+        if not digits:
+            return []
+
+        def dfs(i, path):
+            if i == len(digits):
+                if path:
+                    res.append("".join(path))
+                return
+
+            
+            corresp = mapping[digits[i]]
+            for letter in corresp:
+                path.append(letter)
+                dfs(i + 1, path)
+                path.pop()
+
+        dfs(0, [])
+        return res
