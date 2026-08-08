4d3666ff852650e1ee7098c3147c905121699fd2
Enrique Fernández <73256011+EnriqueFdez03@users.noreply.github.com>
2026-08-08 11:36:08 +0200

    Add: count-number-of-islands - submission-8

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-8.py
@@ -0,0 +1,48 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+    
+    def find(self, u):
+        curr = self.parent[u]
+        while curr != self.parent[curr]:
+            self.parent[curr] = self.parent[self.parent[curr]]
+            curr = self.parent[curr]
+        return curr   
+
+    def union(self, u, v):
+        parentU = self.find(u)
+        parentV = self.find(v)
+        if parentU == parentV:
+            return False
+        
+        if self.rank[parentU] < self.rank[parentV]:
+            self.parent[parentU] = parentV
+        elif self.rank[parentV] > self.rank[parentU]:
+            self.parent[parentV] = parentU
+        else:
+            self.parent[parentU] = parentV
+            self.rank[parentV] += 1
+        return True
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        dsu = DSU(ROWS * COLS)
+
+        islands = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    islands += 1 
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+
+                        if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == "1":
+                            if dsu.union(r * COLS + c, nr * COLS + nc):
+                                islands -= 1
+
+        return islands
+
+
